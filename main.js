b0b1c432064c0b82ca64c4321da5fe7f9f40ed4e
ltm59 <ltm59@drexel.edu>
2021-02-28 01:47:02 -0500

    Load in examples

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ function fade2Content() {
     console.log('fade2Content called');
     document.body.classList.remove("blackScreen");
   }
+
+const modal = document.getElementById('modal');
+
+const btnModal = document.querySelectorAll('.toggle-modal');
+btnModal.forEach(button => {
+  button.addEventListener(
+    'click',
+    () => {
+      modal.hidden = !modal.hidden;
+    },
+    false
+  );
+});
