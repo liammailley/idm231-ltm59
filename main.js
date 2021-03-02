87afbd1cbf64dafeb2ec5d17d2d45565b97c6e3f
ltm59 <ltm59@drexel.edu>
2021-03-02 13:46:14 -0500

    Modal work
    In class today I will decide whether to use modals or have the button transfer to different pages

--- main.js
+++ main.js
@@ -3,12 +3,6 @@ function fade2Content() {
   document.body.classList.remove("blackScreen");
 }
 
-const modal = document.getElementById('modal');
-
-const btnModal = document.querySelectorAll('.toggle-modal');
-btnModal.forEach(button => {button.addEventListener('click', function () { modal.hidden = !modal.hidden; }, false);}
-  );
-
 function original() {
   console.log('Original');
 }
@@ -55,4 +49,33 @@ function baked() {
 
 function kettlecooked() {
   console.log('Kettle Cooked');
+}
+
+/* Modal Code */
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
